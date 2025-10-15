ca9792b272eb5bd1ed7a1b9b8d48684b01d6bf75
emeraudesantu <emeraudesantu05@mail.com>
2025-10-15 13:59:35 -0500

    mon premier commit

---
+++ exercice1.dart
@@ -0,0 +1,25 @@
+ class Couleur {
+
+final int r, v, b ;
+Couleur(this.r, this.v, this.b);
+
+ }
+// Couleur.rouge() r=255,v=0,b=0;
+// Couleur.vert () r=0,v=255,b=0;
+// Couleur.bleu () r=0,v=0,b=255;
+
+// void afficherCouleur () : {
+//     print("R:$r, V:$v, B:$b");
+// }
+//  }
+
+//  void main() {
+
+// class r = Couleur.rouge ();
+// class v = Couleur.vert ();
+// class b = Couleur.bleu ();
+
+// r.afficherCouleur();
+// v.afficherCouleur();
+// b.afficherCouleur();
+//  
