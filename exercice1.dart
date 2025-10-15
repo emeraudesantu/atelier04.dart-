6d50e9b5dec3f65fe710ebfa26feff107383c4fe
emeraudesantu <emeraudesantu05@mail.com>
2025-10-15 14:08:45 -0500

    2

--- exercice1.dart
+++ exercice1.dart
@@ -3,15 +3,15 @@
 final int r, v, b ;
 Couleur(this.r, this.v, this.b);
 
- }
-// Couleur.rouge() r=255,v=0,b=0;
-// Couleur.vert () r=0,v=255,b=0;
-// Couleur.bleu () r=0,v=0,b=255;
 
-// void afficherCouleur () : {
-//     print("R:$r, V:$v, B:$b");
-// }
-//  }
+Couleur.rouge() : r=255,v=0,b=0;
+Couleur.vert () : r=0,v=255,b=0;
+Couleur.bleu () : r=0,v=0,b=255;
+
+void afficherCouleur () {
+    print("R:$r, V:$v, B:$b");
+}
+ }
 
 //  void main() {
 
@@ -22,4 +22,5 @@ Couleur(this.r, this.v, this.b);
 // r.afficherCouleur();
 // v.afficherCouleur();
 // b.afficherCouleur();
-//  
+ 
+//  }
