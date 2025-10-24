6b1b95845a8c81572af1775562e74104b0dd8924
emeraudesantu <emeraudesantu05@mail.com>
2025-10-24 10:09:05 -0500

    3e commit

--- exercice5.dart
+++ exercice5.dart
@@ -18,3 +18,15 @@ class Cercle extends Forme {
     return 3.1416 * rayon * rayon;
   }
 }
+
+class Rectangle extends Forme {
+  final double longueur;
+  final double largeur;
+
+  Rectangle(this.longueur, this.largeur);
+
+  @override
+  double calculerAire() {
+    return longueur * largeur;
+  }
+}
