c12405270160204791afe3b15ec05bb52cf19434
emeraudesantu <emeraudesantu05@mail.com>
2025-10-24 09:46:17 -0500

    3e commit

--- exercice3.dart
+++ exercice3.dart
@@ -10,4 +10,16 @@ class Compte {
   String get solde => "${_solde.toStringAsFixed(2)} \$";
 
 
+  void depot(double montant) {
+    if (montant > 0) {
+      _solde += montant;
+      // Optionnel : afficher confirmation du dépôt
+      print("Dépôt effectué : ${montant.toStringAsFixed(2)} \$");
+    } else {
+      // Montant invalide => ne rien faire et afficher un message d'erreur
+      print("Montant invalide : le dépôt doit être positif.");
+    }
+  }
+
+
 }
