b2ef035abdb8324c8b69ea3d50e9457e2afe5505
emeraudesantu <emeraudesantu05@mail.com>
2025-10-24 10:17:41 -0500

    3e commit

--- exercice6.dart
+++ exercice6.dart
@@ -16,3 +16,16 @@ class ServeurAPI implements Connectable {
     print("ServeurAPI : Déconnexion réussie.");
   }
 }
+
+class BaseDeDonnees implements Connectable {
+ 
+  @override
+  void connecter(String utilisateur) {
+    print("BaseDeDonnees : Connexion ouverte pour $utilisateur.");
+  }
+
+  @override
+  void deconnecter() {
+    print("BaseDeDonnees : Déconnexion effectuée.");
+  }
+}
