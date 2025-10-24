016f407ccfca83cc27bc4f1a81a90f82f0031cce
emeraudesantu <emeraudesantu05@mail.com>
2025-10-24 09:49:04 -0500

    exercice 3

--- exercice3.dart
+++ exercice3.dart
@@ -27,4 +27,23 @@ class Compte {
   }
 }
 
+void main() {
 
+  // Création d'un compte avec numéro "C12345" et solde par défaut 0.0
+  var compte = Compte("C12345");
+
+  // Affichage du solde initial (devrait être "0.00 $")
+  compte.afficherSolde();
+
+  // Effectuer un dépôt positif (ex : 150.0)
+  compte.depot(150.0);
+
+  // Affichage du solde après dépôt (devrait être "150.00 $")
+  compte.afficherSolde();
+
+  // Tentative de dépôt invalide (montant négatif) pour montrer la vérification
+  compte.depot(-20.0);
+
+  // Affichage final du solde (devrait rester "150.00 $")
+  compte.afficherSolde();
+}
